a776aa34f5f485e256a8769a596ed72e817b90da
nag.samayam <nag.samayam@gmail.com>
2019-08-08 20:33:10 +0530

    Add basic configuration

---
+++ src/config/config.service.ts
@@ -0,0 +1,46 @@
+import * as dotenv from 'dotenv';
+import * as Joi from '@hapi/joi';
+import * as fs from 'fs';
+
+export interface EnvConfig {
+    [key: string]: string;
+}
+
+export class ConfigService {
+    private readonly envConfig: EnvConfig;
+
+    constructor(filePath: string) {
+        const config = dotenv.parse(fs.readFileSync(filePath));
+        this.envConfig = this.validateInput(config);
+    }
+
+    /**
+     * Ensures all needed variables are set, and returns the validated JavaScript object
+     * including the applied default values.
+     */
+    private validateInput(envConfig: EnvConfig): EnvConfig {
+        const envVarsSchema: Joi.ObjectSchema = Joi.object({
+            NODE_ENV: Joi.string()
+                .valid(['development', 'production', 'test', 'provision'])
+                .default('development'),
+            APP_PORT: Joi.number().default(3000),
+        });
+
+        const { error, value: validatedEnvConfig } = Joi.validate(
+            envConfig,
+            envVarsSchema,
+        );
+        if (error) {
+            throw new Error(`Config validation error: ${error.message}`);
+        }
+        return validatedEnvConfig;
+    }
+
+    get appEnv(): string {
+        return this.envConfig.NODE_ENV;
+    }
+
+    get appPort(): number {
+        return Number(this.envConfig.APP_PORT);
+    }
+}
